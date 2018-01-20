a974f54a2600ca891e1a2c7be0be3cb70213933b
johngem <johngemegah@aol.com>
2018-01-20 09:19:51 -0500

    prevent .pyc files

--- feed.py
+++ feed.py
@@ -1,5 +1,9 @@
 from utils import populate_stations
 from utils import get_entities_for
+from sys import dont_write_bytecode
+
+#Prevent python from creating .pyc files
+dont_write_bytecode = True 
 
 if __name__ == "__main__":
     stations = populate_stations(get_entities_for("G"))
